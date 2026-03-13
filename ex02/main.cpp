91bbe7bc0bfc8413f2786f1fd0db86c934c1c1de
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-03-13 12:21:49 +0100

    Format main.cpp for consistency and readability

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -17,28 +17,28 @@
 
 int main()
 {
-    Bureaucrat boss("President", 1);
-    Bureaucrat assistant("Assistant", 45);
-    Bureaucrat intern("Intern", 150);
+    Bureaucrat boss("President", 1);
+    Bureaucrat assistant("Assistant", 45);
+    Bureaucrat intern("Intern", 150);
 
-    ShrubberyCreationForm f1("home");
-    RobotomyRequestForm f2("Christian");
-    PresidentialPardonForm f3("Criminal");
+    ShrubberyCreationForm f1("home");
+    RobotomyRequestForm f2("Christian");
+    PresidentialPardonForm f3("Criminal");
 
-    std::cout << "--- Shrubbery Test ---" << "\n";
-    intern.executeForm(f1);
-    assistant.signForm(f1);
-    assistant.executeForm(f1);
+    std::cout << "--- Shrubbery Test ---" << std::endl;
+    intern.executeForm(f1);
+    assistant.signForm(f1);
+    assistant.executeForm(f1);
 
-    std::cout << "\n--- Robotomy Test ---" << "\n";
-    assistant.signForm(f2);
-    assistant.executeForm(f2);
-    boss.executeForm(f2);
+    std::cout << "\n--- Robotomy Test ---" << std::endl;
+    assistant.signForm(f2);
+    assistant.executeForm(f2);
+    boss.executeForm(f2);
 
-    std::cout << "\n--- Pardon Test ---" << "\n";
-    boss.executeForm(f3);
-    boss.signForm(f3);
-    boss.executeForm(f3);
+    std::cout << "\n--- Pardon Test ---" << std::endl;
+    boss.executeForm(f3);
+    boss.signForm(f3);
+    boss.executeForm(f3);
 
-    return 0;
+    return 0;
 }
