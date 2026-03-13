ac3345079795a5869161c6a176d3df514f5dc41d
bravex999 <182925606+bravex999@users.noreply.github.com>
2026-03-13 12:20:29 +0100

    Update main.cpp

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -16,42 +16,42 @@
 
 int main()
 {
-    try
-    {
-        Form invalidHigh("FormA", 0, 50);
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
+    try
+    {
+        Form invalidHigh("FormA", 0, 50);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
-    try
-    {
-        Form invalidLow("FormB", 50, 151);
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
+    try
+    {
+        Form invalidLow("FormB", 50, 151);
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
-    try
-    {
-        Bureaucrat boss("Christian", 1);
-        Bureaucrat intern("Douglas", 150);
-        Form taxForm("TaxForm", 50, 50);
+    try
+    {
+        Bureaucrat boss("Christian", 1);
+        Bureaucrat intern("Douglas", 150);
+        Form taxForm("TaxForm", 50, 50);
 
-        std::cout << taxForm << "\n";
+        std::cout << taxForm << std::endl;
 
-        intern.signForm(taxForm);
-        std::cout << taxForm << "\n";
+        intern.signForm(taxForm);
+        std::cout << taxForm << std::endl;
 
-        boss.signForm(taxForm);
-        std::cout << taxForm << "\n";
-    }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
+        boss.signForm(taxForm);
+        std::cout << taxForm << std::endl;
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
 
-    return 0;
+    return 0;
 }
